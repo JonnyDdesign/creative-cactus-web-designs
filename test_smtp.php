695c8a4f56ff7e36f5247e25da00f82b653b447c
JonnyDdesign <jondesserres@gmail.com>
2024-08-26 11:46:45 -0700

    changed the testing php file

--- test_smtp.php
+++ test_smtp.php
@@ -5,25 +5,19 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
-
 try {
-    //Server settings
     $mail->isSMTP();
-    $mail->Host = 'smtp.titan.email';
+    $mail->Host = 'smtp.titan.email'; // or 'smtp.hostinger.com'
     $mail->SMTPAuth = true;
     $mail->Username = 'info@creativecactuswebdesigns.com';
-    $mail->Password = getenv('SMTP_PASSWORD');
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-    $mail->Port = 465;
+    $mail->Password = 'Dane1011';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
 
-    //Recipients
     $mail->setFrom('info@creativecactuswebdesigns.com', 'Creative Cactus Web Designs');
-    $mail->addAddress('jondesserres@gmail.com'); // Change this to your email for testing
-
-    //Content
-    $mail->isHTML(true);
+    $mail->addAddress('info@creativecactuswebdesigns.com');
     $mail->Subject = 'Test Email';
-    $mail->Body    = 'This is a test email.';
+    $mail->Body    = 'This is a test email sent using PHPMailer.';
 
     $mail->send();
     echo 'Message has been sent';
