9eac076c2909e34b0d07e4e33920eeb4c7b577e3
Jon Desserres <jondesserres@gmail.com>
2025-07-31 11:14:54 -0700

    debug php

--- submit_form.php
+++ submit_form.php
@@ -103,8 +103,10 @@ error_reporting(E_ALL);
 
 echo "Starting test...\n";
 
+echo realpath($dotenv_path);
+
 // Check if .env exists
-$dotenv_path = __DIR__ . '/../.env';
+$dotenv_path = __DIR__ . '/./.env';
 if (!file_exists($dotenv_path)) {
     echo "ERROR: .env file is missing\n";
     exit;
